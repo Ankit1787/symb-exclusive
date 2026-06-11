4a0fe2e28f7ef0def77afe358b5e904a76c49738
ankit chauhan <achauhan1787@gmail.com>
2026-06-11 19:18:35 +0530

    ai asistant

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { Toaster } from 'vue-sonner'
 import 'vue-sonner/style.css'
+import AIShoppingAssistant from './components/AIShoppingAssistant.vue';
 </script>
 <template>
   <div>
      <Toaster />
     <NuxtPage />
+    <AIShoppingAssistant/>
   </div>
 </template>
 
